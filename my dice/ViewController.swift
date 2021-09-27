b64184e550b988ac8b75d3a02ffdcc96afc4d226
Marta Kalichynska <85679027+martsia@users.noreply.github.com>
2021-09-27 16:02:41 +0300

    Initial Commit

---
+++ my dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my dice
+//
+//  Created by Marta Kalichynska on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
